15ed9e7a7f17bc9f5db5fe132c70365e3277133e
arjundawande <arjundawande16@gmail.com>
2025-12-07 16:37:29 +0530

    Add icons to ASSETS array in service worker

--- sw.js
+++ sw.js
@@ -4,7 +4,9 @@ const ASSETS = [
   "/", 
   "./index.html",
   "./manifest.json",
-  "./sw.js"
+  "./sw.js",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
 ];
 
 self.addEventListener("install", (event) => {
