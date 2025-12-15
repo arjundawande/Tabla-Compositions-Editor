6c594abf7220a1a8c5f7acea35825bfbd6cff703
arjundawande <arjundawande16@gmail.com>
2025-12-15 15:50:23 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-//Updated on 14th December 2025
-const CACHE_NAME = "tabla-editor-cache-v6.0";
+//Updated on 15th December 2025
+const CACHE_NAME = "tabla-editor-cache-v7.0";
 
 const ASSETS = [
   "/", 
